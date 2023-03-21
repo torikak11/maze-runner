5c17d0353e548b16ed6d821fd01e2fd3c2975d89
torikak11 <victoriakratzke@gmail.com>
2023-03-21 12:30:04 +0000

    Add Window class to setup GUI

---
+++ window.py
@@ -0,0 +1,23 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.geometry(f"{width}x{height}")
+        self.__root.title("Maze Runner")
+        self.__canvas = Canvas(self.__root)
+        self.__canvas.pack()
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running == True:
+            self.redraw()
+
+    def close(self):
+         self.__running = False
